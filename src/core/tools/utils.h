71c456badff884b5d1010c81619b18c5df28c241
Loudet Julien <loudet.julien@gmail.com>
2015-09-25 00:11:56 +0200

    Added new constant CMD_SIZE.
    This constant is used to create the char arrays that will contain the
    commands executed by the system.

--- src/core/tools/utils.h
+++ src/core/tools/utils.h
@@ -50,6 +50,7 @@
 
 #define DEBUG_LINE           fprintf(stdout, "DEBUG: %i\n", __LINE__)
 #define ARR_SIZE             (256)
+#define CMD_SIZE             (512)
 
 static inline void print_error_info(const char *msg)
 {
